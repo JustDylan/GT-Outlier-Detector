8dceb860b94d4b8e052016540a5c79903e5a7f39
nsturgeo <nickolas.sturgeon@wsu.edu>
2024-11-19 16:42:27 -0800

    UI started

---
+++ Outlier_Detection_UI/Outlier_Detection_UI/Logic/ReadCSVFILE.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Outlier_Detection_UI
+{
+    public class ReadCSVFile
+    {
+        public ReadCSVFile()
+        {
+        }
+
+        public static DataView GetCSVData(string path)
+        {
+            DataTable dataTable = new DataTable();
+            TextFieldParser parser = new TextFieldParser(path);
+
+            parser.SetDelimiters(",");
+
+            if(!parser.EndOfData)
+            {
+
+            }
+
+
+            // Change before done.
+            return null;
+        }
+    }
+}
+
