1bb82123b88e07bcdf904d2ecafe927a715b6b22
Dylan Gyori <gyoridylan@gmail.com>
2024-11-21 19:20:54 -0800

    added ability to execute python scripts in C#

--- Outlier_Detection_UI/Outlier_Detection_UI/MainWindow.xaml.cs
+++ Outlier_Detection_UI/Outlier_Detection_UI/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Python.Runtime;
+using OpenTK.Audio.OpenAL;
 
 namespace Outlier_Detection_UI
 {
@@ -33,17 +35,47 @@ namespace Outlier_Detection_UI
                 WpfPlot1.Plot.Add.Scatter(dataX, dataY);
                 WpfPlot1.Refresh();
             };
+
+            // temporary path to python dll just to get things working
+            string pythonDll = System.IO.Path.GetFullPath("../../../../python_interpreter/python311.dll");
+            Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDll);
+            Runtime.PythonDLL = pythonDll;
+
+
+            // experimental code which runs python code that adds 3 to c# integer, then displays this number in graph
+            int integer = 21;
+            object output = RunPython(
+@"integer=integer+3;
+result=integer
+", integer, "integer", "result");
+            WpfPlot1.Plot.Add.Text(output.ToString(), 0, 0);
+
+            //PythonEngine.RunSimpleString(@"print(""hello world from python!"")");
         }
 
-        // Method to open csv file and process to data grid
-        private void CSV_Click(object sender, RoutedEventArgs e)
+        private object RunPython(string pycode, object parameter, string parameterName, string returnedVariableName)
+        {
+            object returnedVariable = new object();
+            PythonEngine.Initialize();
+            using (Py.GIL())
+            {
+                using (PyModule scope = Py.CreateScope())
+                {
+                    scope.Set(parameterName, parameter.ToPython());
+                    scope.Exec(pycode);
+                    returnedVariable = scope.Get<object>(returnedVariableName);
+                }
+            }
+            return returnedVariable;
+        }
+
+// Method to open csv file and process to data grid
+private void CSV_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFile = new OpenFileDialog();
 
             // Change to open in specific folder later *****
             openFile.Filter = "Csv Files| *.csv";
-
-            openFile.ShowDialog();
 
             if(openFile.ShowDialog() == true) 
             {
