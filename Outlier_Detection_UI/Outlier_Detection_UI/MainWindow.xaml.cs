a65e276f60a6be218e05bdaf94e06a4b3be75d1e
Dylan Gyori <gyoridylan@gmail.com>
2024-12-08 14:59:43 -0800

    integrated autoencoder test in c# code

--- Outlier_Detection_UI/Outlier_Detection_UI/MainWindow.xaml.cs
+++ Outlier_Detection_UI/Outlier_Detection_UI/MainWindow.xaml.cs
@@ -45,9 +45,9 @@ namespace Outlier_Detection_UI
             Runtime.PythonDLL = pythonDll;
 
 
-            string pycode = @"
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+            string trainCode = @"
+#import os
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 import numpy as np
 import tensorflow as tf
@@ -110,10 +110,57 @@ autoencoder.export(""autoencoder"")
 test = ""done""
 ";
 
+            string testCode = @"
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.keras import layers, losses
+from tensorflow.keras.models import Model
+
+import csv
+
+DATA_PATH = ""resources\\DataSet1_Normalized.csv""
+
+print(tf.version.VERSION)
+
+# average squared difference between float arrays
+def vDiff(arr1, arr2):
+	length = min(len(arr1), len(arr2))
+	return sum([((arr1[i] - arr2[i])**2)/length for i in range(0, length)], 0)
+
+
+# read data from csv only including columns X1 through X13
+data = {}
+with open(DATA_PATH) as csvfile:
+	csvreader = csv.reader(csvfile, delimiter="","")
+	raw_data = list(csvreader)
+	
+	# remove header row
+	raw_data = raw_data[1:]
+	
+	# remove first 2 columns and parse floats
+	for i in range(0, len(raw_data)):
+		raw_data[i] = raw_data[i][2:]
+		raw_data[i] = [float(elem) for elem in raw_data[i]]
+		
+	data = np.array(raw_data, dtype=""float32"")
+
+# load autoencoder from model trained by train_autoencoder
+autoencoder = tf.saved_model.load(""autoencoder"")
+#autoencoder = tf.keras.models.load_model(""autoencoder.keras"")
+
+decoded_data = autoencoder.serve(data)
+
+decoded_error = [vDiff(decoded_data[i], data[i]) for i in range(0, len(decoded_data))]
+
+test = ""done""
+";
+
             string pycode2 = @"import os
 test = os.getcwd()";
             System.Diagnostics.Debug.WriteLine("\n\n\n\n\n");
-            System.Diagnostics.Debug.WriteLine(RunPython(pycode, "test"));
+            System.Diagnostics.Debug.WriteLine(RunPython(trainCode, "test"));
+            System.Diagnostics.Debug.WriteLine(RunPython(testCode, "test"));
 
             // experimental code which runs python code that adds 3 to c# integer, then displays this number in graph
             int integer = 21;
