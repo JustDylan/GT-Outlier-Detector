99fcd46f372368c47285c6e1ee795097c2556d59
Dylan Gyori <gyoridylan@gmail.com>
2024-12-08 15:18:35 -0800

    updated autoencoder scripts

--- python_scripts/test_autoencoder.py
+++ python_scripts/test_autoencoder.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
@@ -7,7 +6,7 @@ from tensorflow.keras.models import Model
 
 import csv
 
-DATA_PATH = "DataSet1_Normalized.csv"
+DATA_PATH = "resources\\DataSet1_Normalized.csv"
 
 print(tf.version.VERSION)
 
@@ -33,55 +32,12 @@ with open(DATA_PATH) as csvfile:
 		
 	data = np.array(raw_data, dtype="float32")
 
-#print(type(data))
-#print(np.shape(data))
-
 # load autoencoder from model trained by train_autoencoder
 autoencoder = tf.saved_model.load("autoencoder")
 #autoencoder = tf.keras.models.load_model("autoencoder.keras")
 
-# arificially insert outlier
-data[400][0] += 0.4
 decoded_data = autoencoder.serve(data)
-
-# graph of one decoded row
-
-plt.grid()
-plt.plot(np.arange(13), data[400])
-plt.plot(np.arange(13), decoded_data[400])
-plt.show()
-
-plt.grid()
-plt.plot(np.arange(13), data[500])
-plt.plot(np.arange(13), decoded_data[500])
-plt.show()
-
 
 decoded_error = [vDiff(decoded_data[i], data[i]) for i in range(0, len(decoded_data))]
 
-# X1 vs decoded X1
-
-# original data for X1
-sensor1 = [row[0] for row in data]
-
-# decoded X1
-sensor1_decoded = [row[0] for row in decoded_data]
-
-plt.grid()
-plt.title("Sensor X1")
-
-# original X1 data
-plt.scatter(np.arange(len(sensor1)), sensor1)
-
-# reconstructed X1 data
-plt.scatter(np.arange(len(sensor1)), sensor1_decoded)
-plt.show()
-
-
-# predicted error vs row number
-plt.grid()
-plt.title("Averaged squared diff vs Row #")
-
-plt.scatter(np.arange(len(decoded_error)), decoded_error)
-
-plt.show()
+test = "done"
